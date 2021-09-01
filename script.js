57e30ece9c62a652408ddf52c149956380a69bcd
Dougxiii <84218639+Dougxiii@users.noreply.github.com>
2021-08-31 22:46:20 -0300

    update

---
+++ script.js
@@ -0,0 +1,46 @@
+let prato, sobremesa, bebida;
+        
+
+        //Escolher Lanche
+        function escolherBurguer(){
+            document.getElementById("burguer").style.borderColor = "green";
+            document.getElementById("hotdog").style.borderColor = "white";
+            prato = "Burguer";
+        }
+        function escolherHotdog(){
+            document.getElementById("hotdog").style.borderColor = "green";
+            document.getElementById("burguer").style.borderColor = "white";
+            prato = "HotDog";
+        }
+
+        //Escolher Bebida
+        function escolherRefri(){
+            document.getElementById("refri").style.borderColor = "green";
+            document.getElementById("suco").style.borderColor = "white";
+            bebida = "Refri";
+        }
+            
+        function escolherSuco(){
+            document.getElementById("suco").style.borderColor = "green";
+            document.getElementById("refri").style.borderColor = "white";
+            bebida = "Suco";
+        }
+        
+        //Escolher Sobremesa
+        function escolherPave(){
+            document.getElementById("pave").style.borderColor = "green";
+            document.getElementById("mousse").style.borderColor = "white";
+            sobremesa = "Pave";
+        }
+
+        function escolherMousse(){
+            document.getElementById("mousse").style.borderColor = "green";
+            document.getElementById("pave").style.borderColor = "white";
+            sobremesa = "Mousse";
+        }
+
+        function finalizarPedido(){
+            alert(prato);
+            alert(bebida);
+            alert(sobremesa);            
+        }
